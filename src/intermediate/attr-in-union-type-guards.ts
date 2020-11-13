41062b0b2c2eff2ff5851b13337e7946e49fe54a
adi forkasiewicz <adiforka@gmail.com>
2020-11-13 11:08:57 +0100

    adding 'advanced' type content

---
+++ src/intermediate/attr-in-union-type-guards.ts
@@ -0,0 +1,20 @@
+interface Artist {
+	works: { title: string }[]
+}
+
+interface Citizen {
+	votes: { candidate: string }[]
+}
+type Combinable = Artist | Citizen
+
+const proc = (val: Combinable) => {
+	if ('works' in val) {
+		console.log('yay  ')
+	} else if ('votes' in val) {
+		console.log('always')
+	}
+}
+
+proc({ works: [{ title: 'a vase in the morning' }, { title: 'still nature' }] })
+
+proc({ votes: [{ candidate: 'Jimmy Houston' }, { candidate: 'Vivica Fox' }] })
---
+++ src/intermediate/discriminating-unions.ts
@@ -0,0 +1,41 @@
+// use literal type state and give it to all members of a union type (from TS handbook)
+type NetworkLoadingState = {
+	state: 'loading'
+}
+
+type NetworkFailedState = {
+	state: 'failed'
+	code: number
+}
+
+type NetworkSuccessState = {
+	state: 'success'
+	response: {
+		title: string
+		duration: number
+		summary: string
+	}
+}
+
+type NetworkState =
+	| NetworkLoadingState
+	| NetworkFailedState
+	| NetworkSuccessState
+
+// help the compiler work out that we've used all the possible cases (add default branch with never)
+function logger(networkState: NetworkState) {
+	switch (networkState.state) {
+		case 'loading':
+			return 'downloading...'
+		case 'failed':
+			return `Error ${networkState.code} downloading`
+		case 'success':
+			return `Downloaded ${networkState.response.title} -- ${networkState.response.summary}`
+		default:
+			assertNever(networkState)
+	}
+}
+
+function assertNever(x : never): never {
+	throw new Error('unexpected value ' + x)
+}
